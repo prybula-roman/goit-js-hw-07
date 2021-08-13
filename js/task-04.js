619e5cbc5df4f6a49b1afc9039ff63544e07f767
prybula_roman <roman.bismail@gmail.com>
2021-08-13 14:19:30 +0300

    fixbugs

--- js/task-04.js
+++ js/task-04.js
@@ -2,14 +2,7 @@ const butIncrement = document.querySelector('button[data-action="increment"]');
 const butDecrement = document.querySelector('button[data-action="decrement"]');
 const counterValue = document.querySelector('#value');
 let rez = 0;
-/*
-function increment() {
-  counterValue.textContent = `${++rez}`;
-}
-function decrement() {
-  counterValue.textContent = `${--rez}`;
-}
-*/
+
 butIncrement.addEventListener(
   'click',
   () => (counterValue.textContent = `${++rez}`)
--- js/task-07.js
+++ js/task-07.js
@@ -5,5 +5,5 @@ const vol = document.getElementById('font-size-control');
 vol.value = 0;
 const str = document.getElementById('text');
 vol.addEventListener('input', () => {
-  str.style.fontSize = `${parseInt(defSize) * parseInt(vol.value)}px`;
+  str.style.fontSize = `${parseInt(defSize) * (parseInt(vol.value) + 1)}px`;
 });
